d5930f7ddbbfe06d4dcdbe4536cdb640b675ae06
饭桶吉 <1657210793@qq.com>
2021-12-04 17:20:50 +0800

    通过管道快速排序

--- src/pipLineMain/main.go
+++ src/pipLineMain/main.go
@@ -6,8 +6,16 @@ import (
 )
 
 func main() {
-	p := pipLineStudy.ArraySource(3,2,4,56,7,8)
-	for v := range p{
+	//讲数字放入管道后取值
+	//p := pipLineStudy.ArraySource(3,2,4,56,7,8)
+	//for v := range p{
+	//	fmt.Print(v)
+	//}
+
+	//从管道里面取出数据，然后重新进行排序放入管道里面
+	p1 := pipLineStudy.InMemSort(
+		pipLineStudy.ArraySource(3,2,6,9,2,1))
+	for v := range p1{
 		fmt.Print(v)
 	}
 }
--- src/pipLineStudy/nodes.go
+++ src/pipLineStudy/nodes.go
@@ -1,5 +1,7 @@
 package pipLineStudy
 
+import "sort"
+
 //其中 a ... int表示 定义a参数，有不定个int类型的数据 chan int 定义管道保存类型是a
 func ArraySource( a ...int) chan int {
 	out := make(chan int)
@@ -14,12 +16,25 @@ func ArraySource( a ...int) chan int {
 
 //建立管道并且快速排序
 func InMemSort(in <-chan int) <-chan int{
+	//定义一个管道
 	out := make(chan int)
 	go func() {
 		//读取内存,定义变量a 是一个int 类型的数组
 		a := []int{}
 		for v := range in{
-			a = append(a )
+			//往a数组里面添加元素
+			a = append(a,v)
 		}
+		//排序
+		sort.Ints(a)
+		//输出
+		for _,v := range a{
+			//将来V放入到out 管道里面
+			out <- v
+		}
+		//关闭管道
+		close(out)
 	}()
+	//返回
+	return out
 }
