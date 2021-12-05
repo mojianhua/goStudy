0d8694bc686ccaee785d72f789c816a59a16cd5d
饭桶吉 <1657210793@qq.com>
2021-12-05 11:40:22 +0800

    文件生成，文件读取

--- src/pipLineMain/main.go
+++ src/pipLineMain/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"pipLineStudy"
 )
 
@@ -12,10 +14,64 @@ func main() {
 	//	fmt.Print(v)
 	//}
 
-	//从管道里面取出数据，然后重新进行排序放入管道里面
-	p1 := pipLineStudy.InMemSort(
-		pipLineStudy.ArraySource(3,2,6,9,2,1))
-	for v := range p1{
-		fmt.Print(v)
+	////从管道里面取出数据，然后重新进行排序放入管道里面（外部排序）
+	//p1 := pipLineStudy.InMemSort(
+	//	pipLineStudy.ArraySource(3,2,6,9,2,1))
+	//for v := range p1{
+	//	fmt.Print(v)
+	//}
+
+	////归并排序
+	//p2 := pipLineStudy.Merge(
+	//		pipLineStudy.InMemSort(
+	//			pipLineStudy.ArraySource(6,5,2,6,7,8,7)),
+	//		pipLineStudy.InMemSort(
+	//			pipLineStudy.ArraySource(15,12,13,10,18,6,8,33)))
+	//for v := range p2{
+	//	fmt.Print(v)
+	//}
+
+	////生成随机数
+	//p3 := pipLineStudy.RandomSource(99)
+	//for v := range p3 {
+	//	fmt.Print(v)
+	//}
+
+	//写入大文件
+	const filename  = "small.in"
+	const n  = 64
+
+	//新建文件，返回可用的文件描述
+	file ,err := os.Create(filename)
+	if err != nil{
+		panic(err)
+	}
+
+	//生成随机数
+	p4 := pipLineStudy.RandomSource(n)
+
+	//写入数据文件
+	//包文件描述符，使用缓存机制，提高读写速度
+	writer := bufio.NewWriter(file)
+	pipLineStudy.WriteSink(writer,p4)
+	writer.Flush()
+
+	//上面的文件描述符，offset在末端，不能用于读取
+	//打开文件
+	file,err = os.Open(filename)
+	if err !=nil{
+		panic(err)
+	}
+	defer file.Close()
+
+	//读取数据
+	p5 := pipLineStudy.RandomSourceFile(bufio.NewReader(file),-1)
+	count := 0
+	for v := range p5{
+		fmt.Println(v)
+		count ++
+		if count >= 100{
+			break
+		}
 	}
 }
--- src/pipLineStudy/nodes.go
+++ src/pipLineStudy/nodes.go
@@ -1,8 +1,15 @@
 package pipLineStudy
 
-import "sort"
+import (
+	"encoding/binary"
+	"fmt"
+	"io"
+	"math/rand"
+	"sort"
+)
 
 //其中 a ... int表示 定义a参数，有不定个int类型的数据 chan int 定义管道保存类型是a
+//内部排序
 func ArraySource( a ...int) chan int {
 	out := make(chan int)
 	go func() {
@@ -47,8 +54,63 @@ func Merge(in1,in2 <-chan int)  <-chan int {
 		v2,ok2 := <-in2
 		for ok1 || ok2{
 			if !ok2 || (ok1 && v1 <= v2){
-
+				out <- v1
+				v1,ok1 = <-in2
+			}else{
+				out <- v2
+				v2,ok2 = <-in2
 			}
 		}
+		fmt.Print("合并结束")
+		close(out)
 	}()
+	return out
+}
+
+//随机数生成生成count个int型数据
+func RandomSource(count int) <-chan int  {
+	out := make(chan int)
+	go func() {
+		for i := 0; i < count ;i++{
+			out <- rand.Int()
+		}
+		close(out)
+	}()
+	return out
+}
+
+//读取数据
+//第一个参数是读的来源对象，第二个参数是读取长度，（-1全读），输出一个chan
+func RandomSourceFile(reader io.Reader , chunksize int) <- chan int  {
+	out := make(chan int ,1024)
+	go func() {
+		// 64位系统的int类型大小是64，所以用一个64位buffer = byte(8) * 8
+		buffer := make([]byte,8)
+		// 读取长度的控制变量
+		bytesread := 0
+		for {
+			// n是读取的长度
+			n ,err := reader.Read(buffer)
+			bytesread += n
+			// 可能读取最好4字节数据，nul = EOF,再判读nil
+			if n > 0{
+				// 大端还是小端，发送和接收端统一即可
+				out <- int(binary.BigEndian.Uint64(buffer))
+			}
+			if err !=nil || (chunksize != -1 && bytesread >= chunksize){
+				break
+			}
+		}
+		close(out)
+	}()
+	return out
+}
+
+//写数据，第一个参数写的目的对象，第二个参数是写数据channel
+func WriteSink(wirter io.Writer,in <- chan int)  {
+	for v:= range in{
+		buffer :=make([]byte,8)
+		binary.BigEndian.PutUint64(buffer,uint64(v))
+		wirter.Write(buffer)
+	}
 }
